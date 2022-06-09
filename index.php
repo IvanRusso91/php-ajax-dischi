fbcc700ce1462c3bfe033081417451ca3b675078
IvanRusso91 <iv.russo91@gmail.com>
2022-06-09 17:21:15 +0200

    Milestone1 : completato

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php 
 
+include __DIR__ . '/api.php';
+//Controllo per corretto funzionamento dell'include.
+// echo'<pre>';
+// var_dump($db);
+// echo'</pre>';
 
 ?>
 
@@ -9,9 +14,37 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+  <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+  
+  <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+
+  <link rel="stylesheet" href="css/style-php.css">
   <title>Document</title>
 </head>
 <body>
-  
+  <h1>Collezione della lista dischi</h1>
+  <!-- cards -->
+  <div class="ir-card my-5" >
+    <?php foreach($db as $card => $value):?>
+      <div class="cardimg">
+        <img src="<?php echo $value['poster']?>" alt="<?php echo $value['author']?>">
+      </div>
+      <h5 class="testo">
+        <?php echo $value['title']?>
+      </h5>
+      <p>
+        <?php echo $value['author']?>
+      </p>
+      <p>
+      <?php echo $value['year']?>
+      </p>
+    <?php endforeach; ?>
+    
+  </div>
+  <!-- /card -->
+  <script src="./script.js"></script>
 </body>
 </html>
